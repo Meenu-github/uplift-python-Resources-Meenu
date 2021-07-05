b7c84c5e606afdf085430fea381dfa5ceb30eebc
Meenu Singh <meenubgp005@gmail.com>
2021-07-05 20:26:24 +0530

    count the different data types in a list

---
+++ countDataType.py
@@ -0,0 +1,10 @@
+print("The original list is : " + str(test_list))
+
+intCount = len(list(i  for i in test_list if isinstance(i, int) and ((i!=True) and (i!=False))))
+strCount = len(list(i for i in test_list if isinstance(i, str)))
+boolCount = len(list(i for i in test_list if isinstance(i, bool)))
+floatCount = len(list(i for i in test_list if isinstance(i, float)))
+print("The length of integers in list is : " + str(intCount))
+print("The length of floats in list is : " + str(floatCount))
+print("The length of strings in list is : " + str(strCount))
+print("The length of boolean in list is : " + str(boolCount))
