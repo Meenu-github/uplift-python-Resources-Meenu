a758867782d96a4c1d438ed147d1a8bb93bf40e6
Meenu Singh <meenubgp005@gmail.com>
2021-07-06 10:50:22 +0530

    Counting different data types element and differentiating between 1 and 0 with True and False

--- countDataType.py
+++ countDataType.py
@@ -1,7 +1,8 @@
-test_list = [2, 1.2, 1.4,'hello', 'nice', True, False]
+test_list = [3,1,0,'computer',True, False, 5, 'geeks', 6, 7, 2.3, 6.5]
+
 print("The original list is : " + str(test_list))
 
-intCount = len(list(i  for i in test_list if isinstance(i, int) and ((i!=True) and (i!=False))))
+intCount = len(list(i  for i in test_list if isinstance(i, int) and not isinstance(i, bool)   ))
 strCount = len(list(i for i in test_list if isinstance(i, str)))
 boolCount = len(list(i for i in test_list if isinstance(i, bool)))
 floatCount = len(list(i for i in test_list if isinstance(i, float)))
@@ -9,3 +10,4 @@ print("The length of integers in list is : " + str(intCount))
 print("The length of floats in list is : " + str(floatCount))
 print("The length of strings in list is : " + str(strCount))
 print("The length of boolean in list is : " + str(boolCount))
+
