5ed3c0909c6dab68a44d080dcbf0a2e98338a5c9
Sheelu Singh <singhsheelu2122@gmail.com>
2021-07-24 22:27:24 +0530

    Update

--- Donation/bloodDonation.py
+++ Donation/bloodDonation.py
@@ -7,11 +7,11 @@ ws.title = "blooddonation"
 def bloodDonate():
     st.title("Blood Donation")
     from PIL import Image
-    image = Image.open("https://github.com/Meenu-github/donationApp/blob/main/_bloodDonation.jpg")
+    image = Image.open("E:\Meenu\Snapchat\_bloodDonation.jpg")
     st.image(image, caption='Blood Donation')
     st.write("This is the blood donation page.")
     st.write("Here if you are willing to donate blood\n you have to register yourself.")
-    with st.form(key="Registration for Blood Donation"):
+    with st.form(key="Registration for Blood Donation") :
         bloodname = st.text_input("Enter your name : ")
         bgrp = st.text_input("Enter your blood group : ")
         age = st.slider(label="Enter your age ", min_value=18, max_value=45)
