e635380901065ef8b454ca0c51e2064b6e9a729f
Meenu Singh <meenakshi15sept2000@gmail.com>
2021-07-27 12:26:05 +0530

    Done

--- Donation/firstPage.py
+++ Donation/firstPage.py
@@ -1,8 +1,6 @@
 import streamlit as st
 def firstpage():
-    st.markdown("## Welcome to the Donation page.")
-    st.title("Donation")
-    st.header("Let us come together and donate something for the needy.")
+    pass
     
 
 
